46605970678649ab359206cba568f8737b6bc408
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-15 14:49:11 -0300

    criando a lógica de sucesso ou fracasso

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const buttonReset = document.querySelector(".btn-reset");
 const spanHint = document.querySelector(".span-hint");
 const spanGuess = document.querySelector(".span-guess");
 const spanLetters = document.querySelector(".span-letters");
+const message = document.querySelector(".message");
 
 function getRandomWord() {
   const randomNumber = Math.round(Math.random() * wordList.length);
@@ -21,12 +22,31 @@ function createWord(getWord) {
 
 function createWords() {
   const getWord = getRandomWord();
-  console.log(getWord);
   spanHint.textContent = getWord.hint;
   for (let i = 0; i < getWord.word.length; i += 1) {
     createWord(getWord);
   }
-  console.log(getWord);
+
+  let setChar = document.querySelectorAll(".char");
+
+  document.addEventListener("keydown", (e) => {
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const isEventFinInLetters = letters.includes(e.key);
+    const isEventInWord = getWord.word.includes(e.key);
+
+    if (isEventFinInLetters && isEventInWord) {
+      let findIndex = getWord.word.indexOf(e.key);
+      setChar[findIndex].textContent = e.key;
+    } else {
+      let result = +spanGuess.textContent;
+      spanGuess.textContent = +spanGuess.textContent - 1;
+      if (result === 0) {
+        message.textContent = "😵‍💫 Você perdeu!";
+        return;
+      }
+      spanLetters.textContent = +spanLetters.textContent + 1;
+    }
+  });
 }
 
 createWords();
